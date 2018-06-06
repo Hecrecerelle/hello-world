2611da5c869da8608aabeb0c75e8b40db4df4d86
Hecrecerelle <32090028+Hecrecerelle@users.noreply.github.com>
2018-06-06 22:43:59 +0800

    test

--- SuperPrime.cpp
+++ SuperPrime.cpp
@@ -1,29 +1,47 @@
-class Prime {
+#include <iostream>
+using namespace std;
+
+
+class prime
+{
 public:
-  Prime(int num);
-  ~Prime();
-
-  bool isPrime();
+	prime(int num);//constructed function
+					//initialize the variable "num"
+	~prime();//destructed function
+	bool isprime(int num);//judge prime
+	int sumbit();//return the sum of each bits
+	int multibit();//return the multi of each bits
+	int squaremultibit();//return the suqre of each bits
 private:
-  const int mNum;
-};
-class SuperPrime : public Prime {
-public:
-  SuperPrime(int num);
-  ~SuperPrime();
+	int mnum;
+};	
 
-  bool isPrime();
 
-  Prime sumBit();
-  Prime multiBit();
-  Prime sqaureSumBit();
-private:
-  const int num;
-}
 int main()
 {
-    SuperPrime sp(113);
-    if(sp.isPrime()) {
-        ; // do something
-    }
+	int index;
+	for (index = 100; index <= 9999; index++)
+	{
+		prime number(index);//初始化私有成员  其值为index
+		//调用成员函数isprime判定每一中数据是否为素数
+		if (!(number.isprime(index)))
+		{
+			continue;
+		}
+		if (!(number.isprime(number.multibit())))
+		{
+			continue;
+		}
+		if (!(number.isprime(number.squaremultibit())))
+		{
+			continue;
+		}
+		if (!(number.isprime(number.sumbit())))
+		{
+			continue;
+		}
+		cout << index << endl;
+	}
+	getchar();
+	return 0;
 }
