8da067b7558163c240b919b579cd5b8509b61402
Hecrecerelle <32090028+Hecrecerelle@users.noreply.github.com>
2018-06-07 20:59:38 +0800

    super_prime

--- Table1.cpp
+++ Table1.cpp
@@ -1,38 +1,71 @@
-class Table {
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+class Cell 
+{
+	friend class Table;
 public:
-
-  void show();
-  int addRow();
-  void delRow(int n);
-  int addColumn();
-  void delColumn(int n);
-private:
-  Cell cells;
-};
-class Cell {
-public:
-  Cell(char con[]);
-  ~Cell();
-
-  void show();
+  Cell(string con)
+  {
+  	this->content=con;
+  }
+  Cell()
+  {
+  }
+  ~Cell()
+  {
+  }
+  void show()
+  {
+  	cout<<content<<endl;
+  }
 private:
   string content;
-}
-int main() {
-  Table tb;
-  tb.show();
-  tb.addRow();
-  tb.show();
-  tb.addColumn();
-  tb.show();
-  Table tb1(5,5);
-  tb1.show();
-  tb1.set(1,1,30);
-  tb1.set(2,2,"hello");
-  tb1.show();
-  tb1.delRow(1);
-  tb1.show();
-  tb1.delColumn(1);
-  tb1.show();
+};
+class Table
+{
+public:
+
+  void setRow(int row)
+  {
+  }
+  void show()
+  {
+	cout<<this->cells->content;
+  }
+  int addRow()
+  {
+  }
+  void delRow(int n)
+  {
+  }
+  int addColumn()
+  {
+  }
+  void delColumn(int n)
+  {
+  }
+private:
+	int row;
+  	Cell *cells=new Cell[];
+};
+int main() 
+{
+  	Table tb;
+  	tb.show();
+  	tb.addRow();
+  	tb.show();
+  	tb.addColumn();
+  	tb.show();
+//  Table tb1(5,5);
+//  tb1.show();
+//  tb1.set(1,1,30);
+//  tb1.set(2,2,"hello");
+//  tb1.show();
+//  tb1.delRow(1);
+//  tb1.show();
+//  tb1.delColumn(1);
+//  tb1.show();
   return 0;
 }
