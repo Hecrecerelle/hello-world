136618f1fd1e11faece1e91042029d62e85e433d
Hecrecerelle <32090028+Hecrecerelle@users.noreply.github.com>
2018-06-07 21:22:59 +0800

    prime

--- Prime.cpp
+++ Prime.cpp
@@ -1,23 +1,89 @@
-class Prime {
+#include <iostream>
+using namespace std;
+
+
+class prime
+{
 public:
-  Prime(int num);
-  ~Prime();
-
-  bool isPrime();
-
-  Prime sumBit();
-  Prime multiBit();
-  Prime sqaureSumBit();
+	prime(int num)//constructed function					//initialize the variable "num"
+	{
+		this->num=num;
+	}
+	~prime()//destructed function
+	{
+	}
+	bool isprime(int num)//judge prime
+	{
+		int index;
+		for(index=2;index<this->num;index++)
+		{
+			if(!(this->num%index))
+				return 0;
+		}
+		return 1;
+	}
+	int sumbit()//return the sum of each bits
+	{
+		int a=this->num;
+		int sum=0;
+		while(a)
+		{
+			sum+=(a%10);
+			a/=10;
+		}
+		return sum;
+	}
+	int multibit()//return the multi of each bits
+	{
+		int a=this->num;
+		int mul=1;
+		while(a)
+		{
+			mul*=(a%10);
+			a/=10;
+		}
+	}
+	int squaremultibit()//return the suqre of each bits
+	{
+		int a=this->num;
+		int sqmul=0;
+		while(a)
+		{
+			sqmul+=((a%10)*(a%10));
+			a/=10;
+		}
+	}
 private:
-  const int mNum;
-};
+	int num;
+};	
 
-int main() {
-  Prime pm(113);
-  Prime sb = pm.sumBit();
-  Prime mb = pm.multiBit();
-  Prime ssb = pm.sqaureSumBit();
-  if(pm.isPrime() && sb.isPrime() && mb.isPrime() && ssb.isPrime())
-    ; //to do sth
-  return 0;
+
+
+int main()
+{
+	int index;
+	for (index = 100; index <= 9999; index++)
+	{
+		prime number(index);//初始化私有成员  其值为index
+		//调用成员函数isprime判定每一中数据是否为素数
+		if (!(number.isprime(index)))
+		{
+			continue;
+		}
+		if (!(number.isprime(number.multibit())))
+		{
+			continue;
+		}
+		if (!(number.isprime(number.squaremultibit())))
+		{
+			continue;
+		}
+		if (!(number.isprime(number.sumbit())))
+		{
+			continue;
+		}
+		cout << index << endl;
+	}
+	getchar();
+	return 0;
 }
--- prime.h
+++ prime.h
@@ -5,13 +5,54 @@ using namespace std;
 class prime
 {
 public:
-	prime(int num);//constructed function
-					//initialize the variable "num"
-	~prime();//destructed function
-	bool isprime(int num);//judge prime
-	int sumbit();//return the sum of each bits
-	int multibit();//return the multi of each bits
-	int squaremultibit();//return the suqre of each bits
+	prime(int num)//constructed function					//initialize the variable "num"
+	{
+		this->num=num;
+	}
+	~prime()//destructed function
+	{
+	}
+	bool isprime(int num)//judge prime
+	{
+		int index;
+		for(index=2;index<this->num;index++)
+		{
+			if(!(this->num%index))
+				return 0;
+		}
+		return 1;
+	}
+	int sumbit()//return the sum of each bits
+	{
+		int a=this->num;
+		int sum=0;
+		while(a)
+		{
+			sum+=(a%10);
+			a/=10;
+		}
+		return sum;
+	}
+	int multibit()//return the multi of each bits
+	{
+		int a=this->num;
+		int mul=1;
+		while(a)
+		{
+			mul*=(a%10);
+			a/=10;
+		}
+	}
+	int squaremultibit()//return the suqre of each bits
+	{
+		int a=this->num;
+		int sqmul=0;
+		while(a)
+		{
+			sqmul+=((a%10)*(a%10));
+			a/=10;
+		}
+	}
 private:
-	int mnum;
-};	
+	int num;
+};
