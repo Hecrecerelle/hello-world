4b9e3e8d24f664d517066fc9693cdc448fc3e635
Hecrecerelle <32090028+Hecrecerelle@users.noreply.github.com>
2018-06-11 23:33:27 +0800

    work

--- Chapter2.cpp
+++ Chapter2.cpp
@@ -1,12 +1,27 @@
+#include <iostream>
 #include <string>
-class Chapter2 {
+using namespace std;
+
+//class pages
+//{
+//	private:
+//		int page_no;
+//		int page
+//}
+
+class Chapter2 
+{
 public:
-  Chapter2(){}
-  Chapter2(std::string &title, int pages):pages(pages),title(title) {}
+  Chapter2()
+  {}
+  Chapter2(string &title, int pages):pages(pages),title(title) {}
   void show() const {}
 private:
-  const string title; //章节标题固定的字符串
-  const int pages; //章节页数固定，不能改变
-  //复制章节对象没有意义，所以禁止
+  const string title;
+  const int pages; 
   Chapter2(const Chapter2 &obj) {}
+};
+int main()
+{
+	return 0;
 }
--- Clock.cpp
+++ Clock.cpp
@@ -11,7 +11,11 @@ class Clock
 		void tick(void);
 		void show(int a);
 		void adjust(int year,int month,int day,int hour,int minute,float second);
-		int judge_run(int year);
+		void adjust()
+		{
+			;
+		}
+		void judge_run(int year);
 	private:
 		int year;
 		int month;
@@ -19,6 +23,7 @@ class Clock
 		int hour;
 		int minute;
 		float second;
+		struct tm*local;
 };
 
 
@@ -32,7 +37,7 @@ Clock::~Clock()
 	;
 }
 
-Clock::adjust(int year,int month,int day,int hour,int minute,float second)//illicialize the varible
+void Clock::adjust(int year,int month,int day,int hour,int minute,float second)//illicialize the varible
 {
 	this->year=year;
 	this->month=month;
@@ -40,8 +45,8 @@ Clock::adjust(int year,int month,int day,int hour,int minute,float second)//illi
 	this->hour=hour;
 	this->minute=minute;
 	this->second=second;
-
-int Clock::show(int a)
+}
+void Clock::show(int a)
 {
 	if(a)
 	{
@@ -51,14 +56,42 @@ int Clock::show(int a)
 
 void Clock::judge_run(int year)//判断是否是闰年 
 {
-	
+	if(year%400)
+	{
+		if(!(year%100))
+			cout<<"这一年是平年"<<endl;
+		else
+		{
+			if(year%4)
+				cout<<"这一年是平年"<<endl;
+			else
+				cout<<"这一年是闰年"<<endl;
+		}
+	}
+	else
+	{
+		cout<<"这一年是闰年"<<endl;
+	}
 }
 
-int main() {
+void Clock::tick()
+{
+	time_t it=time(NULL);
+	local=localtime(& it);
+	this->year=local->tm_year+1900;
+	this->month=local->tm_mon+1;
+	this->day=local->tm_mday;
+	this->hour=local->tm_hour;
+	this->minute=local->tm_min;
+	this->second=local->tm_sec;
+}
+
+int main() 
+{
   Clock ck;
-  ck.tick;
-  ck.show();
-  ck.adjust(16,45,00);
-  ck.show();
+  ck.tick();
+  ck.show(1);
+  ck.adjust(18,1,1,16,45,00);
+  ck.show(1);
   return 0;
 }
